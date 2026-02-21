88d893aa2435f675f9432f030fc5eb85453479d4
OrlandoCvs <orlandocs03@outlook.com>
2026-02-20 22:36:27 -0700

    db migration from xampp to a docker container

--- venv/config/db.py
+++ venv/config/db.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base, sessionmaker # <--- Agrega sessionmaker
 from sqlalchemy.schema import MetaData
-
+import os 
+from dotenv import load_dotenv
 # 1. Configuración básica
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/appointmentdb"
+
+load_dotenv()
+
+DB_USER = "root"
+DB_PASS = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 engine = create_engine(DATABASE_URL)
 
 # 2. Crear la fábrica de sesiones (ESTO TE FALTA)
